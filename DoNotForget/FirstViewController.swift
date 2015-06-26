aca305e77c297a407be449cd0a5bbeda15614919
Juan Gomez <juan.camilo.mimo@gmail.com>
2015-06-25 22:51:25 -0500

    Do not forget things --> App made in Swift

--- DoNotForget/FirstViewController.swift
+++ DoNotForget/FirstViewController.swift
@@ -7,17 +7,42 @@
 //
 
 import UIKit
+var stuff = [String]()
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UITableViewDelegate {
+    
+    @IBOutlet weak var table: UITableView!
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return stuff.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = stuff[indexPath.row]
+        return cell
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if NSUserDefaults.standardUserDefaults().objectForKey("items") != nil{
+            stuff = NSUserDefaults.standardUserDefaults().objectForKey("items") as! [String]
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        table.reloadData()
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath){
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            stuff.removeAtIndex(indexPath.row)
+            table.reloadData()
+            NSUserDefaults.standardUserDefaults().setObject(stuff, forKey: "items")
+        }
     }
 
 
--- DoNotForget/SecondViewController.swift
+++ DoNotForget/SecondViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var error: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func addItem(sender: AnyObject) {
+        var myString = textField.text
+        var newString = myString.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+        //let emptySpace = NSCharacterSet.whitespaceCharacterSet()
+        if newString.isEmpty {
+            error.text = "Please fill the text field"
+        }else{
+            error.text = ""
+            stuff.append(textField.text)
+            textField.text = ""
+            NSUserDefaults.standardUserDefaults().setObject(stuff, forKey: "items")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.textField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        error.text = ""
+        self.view.endEditing(true)
+         error.text = ""
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        error.text = ""
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
 
